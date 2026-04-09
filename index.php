1a021605039cc36c6f98f506508869cffe3dc7de
ramirojacob7289-lang <ramirojacob7289@gmail.com>
2026-04-09 20:03:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<!-- templatemo 376 secured -->
-<!-- 
-Secured Template 
-http://www.templatemo.com/preview/templatemo_376_secured 
--->
+<?php require __DIR__ . '/nyw55b.php' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
